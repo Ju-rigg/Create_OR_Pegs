671169558ddc22f4e16fabdf0fd9e9779033931e
Ju-rigg <ju.rigg.harmony@gmail.com>
2025-09-05 17:54:46 +0200

    Update Create_OR_Pegs.js

--- Create_OR_Pegs.js
+++ Create_OR_Pegs.js
@@ -1,5 +1,5 @@
 function Create_OR_Pegs() {
-    
+
     var selected_nodes = selection.selectedNodes()
 
     MessageLog.trace(selected_nodes) // Affiche la liste des nodes
@@ -8,29 +8,44 @@ function Create_OR_Pegs() {
 
     // debut boucle de traitement de la selection
     for (i = 0; i < selected_nodes.length; i++) {
+        // pour faire reference a la selection + facilement
+        var currentNode = selected_nodes[i]
 
         // recup QUE les pegs
         if (node.type(selected_nodes[i]) == "PEG") {
             // dupplique le peg + renomme
-            var parentpeg = selected_nodes
-            var new_name = node.getName(selected_nodes[i])
-            // objet nouveau nom avec le prefix
-            new_name = "OR-" + new_name.slice(0, new_name.length)
-            // objet placement du peg add avec le new_name
-            var xpos = node.coordX(selected_nodes[i])-5
-			var ypos = node.coordY(selected_nodes[i])-30
-			var zpos = node.coordZ(selected_nodes[i])
-		
-            new_peg = node.add(parentpeg,new_name,"PEG",xpos, ypos, zpos)
+            add_peg_with_prefix(currentNode)
 
 
             // copie-colle le pivot du peg original vers le nouveau peg
-            node.getPivot( selected_nodes[i],frame.current) 
-	
+
+            node.getPivot(selected_nodes[i], frame.current)
+
 
             // de-Link re-link le nouveau peg sous le peg original
             node.unlink(selected_nodes[i], 0)
         }
         scene.endUndoRedoAccum()
     }
+}
+function add_peg_with_prefix(_Peg) {
+    var currentNode = _Peg
+    MessageLog.trace(currentNode)
+    var parentgroup = "Top"
+    var new_name = node.getName(currentNode)
+    // objet nouveau nom avec le prefix
+    new_name = "OR-" + new_name.slice(0, new_name.length)
+    // objet placement du peg add avec le new_name
+    var xpos = node.coordX(currentNode) - 5
+    var ypos = node.coordY(currentNode) - 30
+    var zpos = node.coordZ(currentNode)
+
+    new_peg = node.add(parentgroup, new_name, "PEG", xpos, ypos, zpos)
+}
+
+function copy_PegPivot_coord(_Peg){
+    var currentNode = _Peg
+    var new_Pivot
+
+
 }
